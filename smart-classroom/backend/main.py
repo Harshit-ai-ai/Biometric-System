126a3d4d537952dfa6148628eb460128d07126a5
Harshit Sachdeva <harshit.17apr@gmail.com>
2026-08-11 16:03:35 +0530

    vercel connection

--- smart-classroom/backend/main.py
+++ smart-classroom/backend/main.py
@@ -34,7 +34,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://biometric-system-rho.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
